ce66eae72a7f7c48737dcd6460e55b77f6d8f357
hshziwo <hshziwo@gmail.com>
2023-11-17 18:39:51 +0900

    select 진행중

---
+++ 문자열 포함 조건, 반올림 함수.sql
@@ -0,0 +1,8 @@
+select a.rest_id, a.rest_name, a.food_type, a.favorites, a.address, b.score
+from rest_info a , 
+(SELECT rest_id, round(avg(review_score),2) as score -- 소수점 세번째에서 반올림해라 == 소수점 두번째까지만 나타내라
+from rest_review
+group by rest_id) b
+where a.rest_id = b.rest_id
+and a.address like '서울%' --서울로 시작하기 때문에 %서울% 이 아니라 서울%이어야 함.
+order by score desc, favorites desc
