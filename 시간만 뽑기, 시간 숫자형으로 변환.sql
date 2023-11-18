756474d62ed087b96c0af1f9969780009ac116d3
hshziwo <hshziwo@gmail.com>
2023-11-18 17:35:54 +0900

    group by 진행중

--- 시간만 뽑기, 시간 숫자형으로 변환.sql
+++ 시간만 뽑기, 시간 숫자형으로 변환.sql
@@ -7,3 +7,11 @@ from animal_outs
 where hour between 9 and 19
 group by hour 
 order by hour asc
+
+
+-- 제대로된 정답
+SELECT to_number(to_char(DATETIME,'hh24')) as HOUR, count(*) as COUNT
+from ANIMAL_OUTS 
+where to_char(DATETIME,'hh24:mi') between '09:00' and '19:59'
+group by to_char(DATETIME,'hh24') 
+order by HOUR;
---
+++ 큰따옴표 작은따옴표 쓰임 차이.sql
@@ -0,0 +1,10 @@
+-- 날짜는 작은 따옴표 되고 알리아스는 큰따옴표로 해야되나?
+-- 맞네 블로그 확인
+-- 컬럼명을 쓸때는 쌍따옴표, 문자열은 홑따옴표 !
+-- https://velog.io/@sangzoon0102/Oracle-SQL%EB%AC%B8%EC%9D%98-%EC%8C%8D%EB%94%B0%EC%98%B4%ED%91%9C-%ED%99%91%EB%94%B0%EC%98%B4%ED%91%9C-%EC%A0%95%EB%A6%AC
+-- https://codebb.tistory.com/15
+SELECT MCDP_CD as "진료과코드", count(*) as "5월예약건수"
+from APPOINTMENT
+where to_char(APNT_YMD, 'yyyy-mm') = '2022-05'
+group by MCDP_CD
+order by "5월예약건수" asc, "진료과코드" asc
