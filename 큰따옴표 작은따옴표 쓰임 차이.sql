52dd573e51ae07d8e432e2807dac196feb69bf6d
hshziwo <hshziwo@gmail.com>
2023-11-18 18:01:31 +0900

    group by 진행중

--- 큰따옴표 작은따옴표 쓰임 차이.sql
+++ 큰따옴표 작은따옴표 쓰임 차이.sql
@@ -1,6 +1,7 @@
 -- 날짜는 작은 따옴표 되고 알리아스는 큰따옴표로 해야되나?
 -- 맞네 블로그 확인
 -- 컬럼명을 쓸때는 쌍따옴표, 문자열은 홑따옴표 !
+-- 대문자 이슈가 있어서 한글일때만 쌍따옴표 써서 쓰는걸로!
 -- https://velog.io/@sangzoon0102/Oracle-SQL%EB%AC%B8%EC%9D%98-%EC%8C%8D%EB%94%B0%EC%98%B4%ED%91%9C-%ED%99%91%EB%94%B0%EC%98%B4%ED%91%9C-%EC%A0%95%EB%A6%AC
 -- https://codebb.tistory.com/15
 SELECT MCDP_CD as "진료과코드", count(*) as "5월예약건수"
