42ba755aa1bca40fd5e16178266da83b9d701a41
hshziwo <hshziwo@gmail.com>
2023-11-22 16:43:01 +0900

    String, Date 완료

--- 문자열 자르기.sql
+++ 문자열 자르기.sql
@@ -1,6 +1,8 @@
 -- substr 문자열자르기 상품 카테고리 앞에서 두 글자 예시
--- substr(글자, 시작(1부터시작), 종료)
--- 끝까지 자를꺼면 종료 없어도 됨
+-- substr(글자, 시작(1부터시작), 몇개가져올것인가)
+-- 끝까지 자를꺼면 몇개가져올것인가 없어도 됨
+-- substr 주의할 점은 3번째인수는 몇자리 2번째 인수부터 몇자리 얻어올것인지 하는 것
+-- 즉 substr(tlno,4,4)는 4번째자리부터 4개만 얻어와라는 뜻 헷갈리면 안됨
 
 SELECT substr(product_code,1,2) as category, count(*) as products
 from product
---
+++ 하이픈 - 넣어서 전화번호 만드는법 주소 공백있게 만드는법.sql
@@ -0,0 +1,22 @@
+-- 주소 || 으로 합치는데 중간에 ' ' 공백을 넣어줘서 주소 모양 나오게 하기 결과는
+-- 성남시 분당구 정자일로 210 4동 501호 이런 모양됨
+-- (city||' '|| STREET_ADDRESS1 ||' '|| STREET_ADDRESS2) as "전체주소"
+-- 전화번호는 substr로 자르고 ||으로 중간에 '-' 넣어서 붙이기 결과는
+-- https://siadaddy-cordinglife.tistory.com/5
+-- 010-2203-9488 이렇게 됨
+-- (substr(tlno,1,3)||'-'||substr(tlno,4,4)||'-'||substr(tlno,8)) as "전화번호"
+-- 끝에는 마지막까지 가져올 거라 3번째인수 없음
+
+-- substr 주의할 점은 3번째인수는 몇자리 2번째 인수부터 몇자리 얻어올것인지 하는 것
+-- 즉 substr(tlno,4,4)는 4번째자리부터 4개만 얻어와라는 뜻 헷갈리면 안됨
+
+select user_id, nickname, (city||' '|| STREET_ADDRESS1 ||' '|| STREET_ADDRESS2) as "전체주소", (substr(tlno,1,3)||'-'||substr(tlno,4,4)||'-'||substr(tlno,8)) as "전화번호"
+from USED_GOODS_USER
+where user_id in
+(
+SELECT writer_id
+from USED_GOODS_BOARD
+group by writer_id
+having count(*) >= 3
+)
+order by user_id desc
